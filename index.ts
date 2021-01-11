d46e3cac7787ebffc90b146ac3a1a8c3fe5cc421
puneeth <puneethmotukuru@gmail.com>
2021-01-11 23:20:58 +0530

    Add postgres

--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ import { users } from "./entities/users"
 const cors=require("cors")
 const app=express()
 app.use(cors())
+app.use(express.json())
 const server=app.listen(3000,()=>{
     console.log("server running at 3000....")
 })
@@ -18,21 +19,42 @@ connection.then(
             res.send(users)
         })
         app.post("/api/users",async (req,res)=>{
+            
+            console.log("body",req.body)
             const user=await usersRepository.create(req.body)
             const results = await usersRepository.save(user);
+            
       res.json({
         message: "success",
         payload: results
       });
+    })
       app.get("/api/users/:id",async(req,res)=>{
-          const user=await usersRepository.findOne(req.params.id)
+        console.log("called")
+          console.log(req.params.id)
+          const user=await usersRepository.findOne({where: { id: req.params.id }})
           res.json({
               message:"success",
               payload: user
           })
       })
-
+      app.delete("/api/users/:id",async(req,res)=>{
+        const user=await usersRepository.delete(req.params.id)
+        res.json({
+            message:"success",
         })
+    })
+    app.put("/api/users/:id",async(req,res)=>{
+        const user=await usersRepository.findOne(req.params.id)
+        usersRepository.merge(user, req.body);
+        const result = await usersRepository.save(user);
+        res.json({
+            message:"success",
+            payload:result
+        })
+  
+    })
+        
     }
 ).catch(error=>{
     console.log(error)
